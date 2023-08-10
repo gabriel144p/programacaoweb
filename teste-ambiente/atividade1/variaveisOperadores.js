350f66c6572564a2c90fa53ba6f9ad8021ac1910
Gabriel Cardoso <74602964+gabriel144p@users.noreply.github.com>
2023-08-10 10:29:07 -0300

    atividade 1

---
+++ teste-ambiente/atividade1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+var numero1 = 2;
+var numero2 = 3;
+
+var soma = numero1 + numero2;
+var sub = numero1 - numero2;
+var div = numero1 / numero2;
+var multi = numero1 * numero2;
+
+console.log(numero1)
+console.log(numero2)
+console.log(soma)
+console.log(sub)
+console.log(div)
+console.log(multi)
